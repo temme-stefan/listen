c24bd1d117d788f0971880527ab027d26b43fa93
Stefan Temme <temme@objectcode.de>
2025-03-16 14:36:46 +0100

    first try

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+ssh u106909590@access895651998.webspace-data.io "rm -rf ~/staticSites/liste/artur2025/*" && scp -r index.html img "u106909590@access895651998.webspace-data.io:~/staticSites/liste/artur2025/"
