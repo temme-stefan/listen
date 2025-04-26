45c451e074ea4591f5f57eaa1930d44fc929ebc0
Stefan Temme <temme@objectcode.de>
2025-04-26 16:12:13 +0200

    mutliple targets

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,64 @@
-#/bin/bash
+#!/bin/bash
 
-# Define input files
-HTML_FILE="index.html"
-CSV_FILE="links.csv"
+# Konfiguration
+TEMPLATE_FILE="template/index.html"
+TEMPLATE_IMG_DIR="template/img"
+SSH_USER="u106909590"
+SSH_HOST="access895651998.webspace-data.io"
+BASE_REMOTE_PATH="~/staticSites/liste"
 
-# Define start and end markers for the replacement
+# Start- und End-Marker für den Ersatz
 START_MARKER="\/\/startData"
 END_MARKER="\/\/endData"
 
-# Read content of links.csv and wrap it with backticks
-CSV_CONTENT=$(cat "$CSV_FILE")
-WRAPPED_CONTENT="\`$CSV_CONTENT\`"
+# Funktion für das Deployment
+deploy() {
+    local SOURCE_DIR=$1
+    local TARGET_DIR=$2
+    local TITLE=$3
+    
+    echo "Starte Deployment für $SOURCE_DIR nach $TARGET_DIR"
+    
+    # Erstelle Build-Verzeichnis und Build-Image-Verzeichnis
+    mkdir -p "$SOURCE_DIR/build"
+    mkdir -p "$SOURCE_DIR/build/img"
+    
+    # Kopiere Template in Build-Verzeichnis
+    cp "$TEMPLATE_FILE" "$SOURCE_DIR/build/index.html"
+    
+    # Kopiere Template-Bilder in Build-Image-Verzeichnis
+    cp "$TEMPLATE_IMG_DIR"/* "$SOURCE_DIR/build/img/"
+    
+    # Kopiere spezifische Bilder aus dem img-Verzeichnis
+    cp "$SOURCE_DIR/img"/* "$SOURCE_DIR/build/img/"
+    
+    # Ersetze den Titel
+    sed -i "s/%title/$TITLE/g" "$SOURCE_DIR/build/index.html"
+    
+    # Lese CSV-Inhalt und wrappe ihn
+    CSV_CONTENT=$(cat "$SOURCE_DIR/links.csv")
+    WRAPPED_CONTENT="\`$CSV_CONTENT\`"
+    
+    # Ersetze den Inhalt zwischen den Markern
+    sed -i "/$START_MARKER/,/$END_MARKER/ {
+        /$START_MARKER/ {p; r /dev/stdin
+        d}
+        /$END_MARKER/ p
+        d
+    }" "$SOURCE_DIR/build/index.html" <<< "$WRAPPED_CONTENT"
+    
+    # Deploy auf Server
+    ssh $SSH_USER@$SSH_HOST "rm -rf $BASE_REMOTE_PATH/$TARGET_DIR/*" && \
+    scp -r "$SOURCE_DIR/build/index.html" "$SOURCE_DIR/build/img" "$SSH_USER@$SSH_HOST:$BASE_REMOTE_PATH/$TARGET_DIR/"
+    
+    if [ $? -eq 0 ]; then
+        echo "Deployment für $SOURCE_DIR erfolgreich abgeschlossen"
+    else
+        echo "Fehler beim Deployment von $SOURCE_DIR"
+        exit 1
+    fi
+}
 
-# Use sed to replace the content between the markers
-sed -i "/$START_MARKER/,/$END_MARKER/ {
-    /$START_MARKER/ {p; r /dev/stdin
-    d}
-    /$END_MARKER/ p
-    d
-}" "$HTML_FILE" <<< "$WRAPPED_CONTENT"
-
-
-
-ssh u106909590@access895651998.webspace-data.io "rm -rf ~/staticSites/liste/artur2025/*" && scp -r index.html img "u106909590@access895651998.webspace-data.io:~/staticSites/liste/artur2025/"
+# Führe Deployments nacheinander aus
+deploy "artur" "artur2025" "Arturs Geburtstagsliste"
+deploy "robin" "robin2025" "Robins Geburtstagsliste"
