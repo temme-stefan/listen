ec379fdc0e702d3259fe5faea4d6c561ee71263a
Stefan Temme <temme@objectcode.de>
2026-03-22 21:20:07 +0100

    switched path

--- listengenerator/frontend/vite.config.ts
+++ listengenerator/frontend/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/public/',
   plugins: [react()],
   build: {
-    base: '/listengenerator/public/',
     outDir: '../public',
     emptyOutDir: true,
   },
